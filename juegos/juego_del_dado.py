42d8fca3b17ccc041dbeaccc2f25d69225e174bb
JoaquinAPh <91762475+JoaquinAPh@users.noreply.github.com>
2024-03-14 13:48:36 +0000

    juegos cachipun y juego del dado

--- juegos/juego_del_dado.py
+++ juegos/juego_del_dado.py
@@ -1,8 +1,20 @@
+import random
+
 def juego_del_dado():
-    """
-    Esta función tiene que pedirle al usuario que aprete enter para que lance un dado.
-    Esto genera un número al azar que se le suma a la puntuación del usuario.
-    Después el computador también tiene que lanzar un dado.
-    El primero en sumar 30 puntos gana.
-    """
-    pass
+    j1 = random.randint(1, 6)
+    bot = random.randint(1, 6)
+    return [j1,bot]
+
+j1 = 0
+bot = 0
+while j1 < 30 and bot < 30:
+    x = juego_del_dado()
+    j1 += x[0]
+    bot += x[1]
+
+if j1 >= 30:
+    print("¡Ganaste!")
+else:
+    print("¡Perdiste!")
+print("Puntos jugador:", j1)
+print("Puntos bot:", bot)
