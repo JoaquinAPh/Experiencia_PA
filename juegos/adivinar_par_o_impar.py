fb0c2b38e6916a7235bc5e5f92af609bb8c03900
ClementeAlday <163307435+ClementeAlday@users.noreply.github.com>
2024-03-14 18:24:33 +0000

    juegos 4 y 5 listos

--- juegos/adivinar_par_o_impar.py
+++ juegos/adivinar_par_o_impar.py
@@ -5,4 +5,12 @@ def adivinar_par_o_impar():
     generar un número aleatorio para ver si es par o impar.
     Se debe mostrar si el usuario adivina correctamente o no.
     """
-    pass
+    import random
+    numero = random.randint(1,100)
+    adivina = input("Adivina si estoy pensando en un número par o impar: ")
+
+    if (numero % 2 == 0 and adivina == "par") or (numero % 2 != 0 and adivina == "impar"):
+        print ("ESO ES! Crack")
+    else:
+        print("Pusha no, era el otro")
+    print(f"El número era {numero}")
--- juegos/memoria.py
+++ juegos/memoria.py
@@ -5,4 +5,15 @@ def memoria():
     Luego, debes pedir al usuario que repita la secuencia.
     Se debe mostrar un mensaje si el usuario acierta o no.
     """
-    pass
+    import random
+    num1 = random.randint(0,9)
+    num2 = random.randint(0,9)
+    num3 = random.randint(0,9)
+    print(f"{num1} {num2} {num3}")
+    res1 = int(input())
+    res2 = int(input())
+    res3 = int(input())
+    if res1 == num1 and res2 == num2 and res3 == num3:
+        print("GUAU! Que buena memoria!")
+    else:
+        print("CUEK, falta chocolate para la memoria")
